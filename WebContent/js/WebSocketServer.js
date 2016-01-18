91bfd7d454f3259ed8f8d9322a8aa5670da684b0
MartinNoc <martin.nocker@student.uibk.ac.at>
2016-01-18 02:16:08 +0100

    Chat via HTML5 Websockets

---
+++ WebContent/js/WebSocketServer.js
@@ -0,0 +1,22 @@
+var ws = require("nodejs-websocket")
+
+var clients = [];
+
+var server = ws.createServer(function (conn) {
+	console.log("New connection")
+	clients.push(conn);
+	
+	conn.on("text", function (str) {
+		//console.log("Received "+str)
+		//conn.sendText(str)
+		clients.forEach(function(client) {
+		      client.send(str);
+		});
+	})
+	
+	conn.on("close", function (code, reason) {
+		console.log("Connection closed");
+		var index = clients.indexOf(conn);
+		clients.splice(index,1);
+	})
+}).listen(8080)
